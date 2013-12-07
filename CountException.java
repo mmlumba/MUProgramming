1c5d7d089aabb481f2c1e1f7242be93d941986fa
M Lumba <mmlumba@gmail.com>
2013-12-06 22:43:39 -0500

    delete countexception since that's not in pizzaorder5

--- CountException.java
+++
@@ -1,8 +0,0 @@
-public class CountException extends Exception {
-  public CountException() {
-    super("Error: More than 5 pizzas ordered!!!");
-  }
-  public CountException(String message) {
-    super(message);
-  }
-}
